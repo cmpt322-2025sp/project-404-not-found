bf490dd5535ae1a67ddbffede5bba519f03178c6
Scottd28 <ar0613@westminsteru.edu>
2025-02-17 11:01:31 -0700

    scott: added a screeen for the Grocery Store Selection Part of the game

---
+++ frontend/src/components/games/GrocerySelection.jsx
@@ -0,0 +1,10 @@
+
+const GrocerySelection = () => {
+    return (
+        <div>
+            Front page of the Grocery Store Selection MiniGame :D .
+        </div>
+    )
+}
+
+export default GrocerySelection
