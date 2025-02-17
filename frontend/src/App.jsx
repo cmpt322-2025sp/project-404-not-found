e9445beae0ec5b3d3021f45c71a5ce920cc7dbb9
Arogya <109647530+uparogya@users.noreply.github.com>
2025-02-17 10:33:16 -0700

    arogya: created react app

---
+++ frontend/src/App.jsx
@@ -0,0 +1,11 @@
+import TestGame from "./components/games/TestGame";
+
+function App() {
+  return (
+    <div>
+      <TestGame></TestGame>
+    </div>
+  );
+}
+
+export default App;
---
+++ frontend/src/components/games/TestGame.jsx
@@ -0,0 +1,9 @@
+const TestGame = () => {
+    return (
+        <div>
+            This is test game.
+        </div>
+    )
+}
+
+export default TestGame
