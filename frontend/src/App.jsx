e5c995501f2b8d39f077cfc8b70555e286dc9958
Arogya <109647530+uparogya@users.noreply.github.com>
2025-02-17 20:42:00 -0700

    arogya: added character and character movement in the playground

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,43 @@
 import TestGame from "./components/games/TestGame";
+import React, { useState } from "react"
+import Playground from "./components/Playground"
+import Character from "./components/Character"
+import { character_speed, character_dimension } from "./Const"
 
-function App() {
+const App = () => {
+
+  const [character_position, setCharacterPosition] = useState({ top: 500, left: 500 })
+
+  const movePlayer = (direction) => {
+      setCharacterPosition((prevPosition) => {
+          const { innerWidth, innerHeight } = window
+          const newPosition = { ...prevPosition }
+
+          switch (direction) {
+              case "ArrowUp":
+                  if (prevPosition.top > 10) newPosition.top -= character_speed
+                  break
+              case "ArrowDown":
+                  if (prevPosition.top < innerHeight - character_dimension - 10) newPosition.top += character_speed
+                  break
+              case "ArrowLeft":
+                  if (prevPosition.left > 10) newPosition.left -= character_speed
+                  break
+              case "ArrowRight":
+                  if (prevPosition.left < innerWidth - character_dimension - 10) newPosition.left += character_speed
+                  break
+              default:
+                  break
+          }
+
+          return newPosition
+      })
+  }
+
   return (
-    <div>
-      <TestGame></TestGame>
-    </div>
+    <Playground>
+		<Character position={character_position} movePlayer={movePlayer} ></Character>
+	</Playground>
   );
 }
 
---
+++ frontend/src/Const.jsx
@@ -0,0 +1,4 @@
+const character_speed = 15
+const character_dimension = 150
+
+export { character_speed, character_dimension }
